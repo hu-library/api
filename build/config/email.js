d7cfbce5e6c8a24cef543d2fb38c5b97bf545f13
Judah Goff <bgoff1@harding.edu>
2019-01-29 11:51:03 -0600

    Modify .gitignore, added build to commit

---
+++ build/config/email.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.user = process.env.EMAIL_USERNAME || '';
+exports.pass = process.env.EMAIL_PASS || '';
+exports.to = 'bgoff1@harding.edu';
+//# sourceMappingURL=email.js.map
---
+++ build/config/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const google_sheets_manager_1 = require("google-sheets-manager");
+const credentials_1 = __importDefault(require("./credentials"));
+exports.serverPort = process.env.PORT || 8000;
+exports.logFormat = 'dev';
+exports.backendURL = `http://localhost:${exports.serverPort}/`;
+const spreadsheetID = '1HgFqak5AUZBnSCHBkKvKiGsHw8TEq7MVLWdW5SP6Eks';
+const mainSheetID = 1128497297;
+const colorKeySheetID = 539679140;
+const attributeSheetID = 1037601079;
+exports.authClass = new google_sheets_manager_1.ServiceAccount(credentials_1.default);
+exports.sheetsAPI = new google_sheets_manager_1.GoogleSheet(exports.authClass, spreadsheetID, mainSheetID);
+exports.defaultCallbackForAPI = (err, res) => {
+    if (err) {
+        console.log(`ERROR -- ${err}`);
+        throw err;
+    }
+    console.log(res);
+};
+exports.pageTwoSheetsAPI = new google_sheets_manager_1.GoogleSheet(exports.authClass, spreadsheetID, colorKeySheetID);
+exports.attributeSheetsAPI = new google_sheets_manager_1.GoogleSheet(exports.authClass, spreadsheetID, attributeSheetID);
+exports.subjectStart = 'Library book searching bot: ';
+//# sourceMappingURL=index.js.map
